54d1322d058b504d3dd706f8ba891ee3aefbc655
iresuji <iresuji@hotmail.com>
2023-01-15 23:42:38 +0000

    functions to start button added

--- js/logic.js
+++ js/logic.js
@@ -1,100 +1,61 @@
-// Create a code quiz that contains the following requirements:
-// A start button that when clicked a timer starts and the first question appears.
+//VARIABLE NEEDED
+var startButton = document.getElementById("start")
+
+var quizQuestions = document.getElementById("questions");
+var startScreen = document.getElementById("start-screen");
+var timer = document.getElementById("timer"); // Timer Variable
+var quizQuestionTitle = document.getElementById("question-title")
+
+//FUNCTIONS
 
 
-// Questions contain buttons for each answer.
-// When answer is clicked, the next question appears
-// If the answer clicked was incorrect then subtract time from the clock
-// The quiz should end when all questions are answered or the timer reaches 0.
-// When the game ends, it should display their score and give the user the ability to save their initials and their score
+// Start quiz
+function startQuiz() {
+    startScreen.style.display = "none"; // Hide Rules 
+    quizQuestions.style.display = "block"; // Show Quiz Questions Page
 
-// Prepare the questions in question.js file
-// var questions = ...
-var score = 0;
-var currentQuestion = 0;
-var counter;
-var timer;
+    secondsLeft = 80; // seconds in Timer 
 
-// Prepare all selector that we might need to point to the html element
-var startButton = document.querySelector('#start');
-// startScreenElement
-var startScreenElement = document.querySelector('#start-screen');
-// questionsContainer
-var questionsContainer = document.querySelector('#questions');
-// questionsTitle
-var questionsTitle = document.querySelector('#question-title');
-// choicesContainer
-var choicesContainer = document.querySelector('#choices');
-// timerContainer
-var timerContainer = document.querySelector('#time');
-// ....
-
-function populateQuestion(question) {
-    var question = question.title;
-    var choices = question.choices;
-
-    choicesContainer.innerHTML = '';
-    // questionTitle.textContent = question;
-    var choicesList = document.createElement('ul');
-    for (let i = 0; i < choices.length; i++) {
-        var choice = document.createElement('li');
-        choice.textContent = choices[i];
-        choicesList.appendChild(choice);
-    }
-    // choicesContainer.appendChild(choicesList)
-}
-
-function endGame() {
-    // When the game ends, it should display their score and give the user the ability to save their initials and their score
-
-    // hide questions container
-    // show endScreen container
-    // assign score to finalScore container
-    // reset the timer clearInterval(timer);
-}
-
-function nextQuestion() {
-    currentQuestion++;
-    if (currentQuestion < questions.length) {
-        populateQuestion(questions[currentQuestion]);
-    } else {
-        endGame();
-    }
-}
-
-startButton.addEventListener('click', function () {
-    startScreenElement.setAttribute('class', 'hide');
-    questionsContainer.setAttribute('class', 'visible');
-
-    // show the first question
-    currentQuestion = 0;
-    populateQuestion(questions[currentQuestion]);
-
-    counter = 100;
-    timer = setInterval(function () {
-        counter--;
-        // set timerContainer text to counter
-        if (counter <= 0) {
-            // endGame()
-            clearInterval(timer);
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timer.textContent = "Time: " + secondsLeft;
+        if (secondsLeft === 0 || quizQuestions.length === questionIndex) {
+            clearInterval(timerInterval);
+            showFinalScore();
         }
     }, 1000);
-});
-
-function saveHighscore(initial) {
-    // get the current highscores value from localstorage
-    // json parse current highscores from localstorage, this will be an array of object
-    // push initial + score to the array
-    // order the array from highest score to lowest
-    // json stringify then save back to localstorage
 }
 
-// Another click event listener for choices
-//    Check answer
-//        if correct, add 1 to score, call nextQuestion()
-//        if wrong, remove 10 seconds from the interval, call nextQuestion()
+// First page 
+function codeQuizChallenge() {
+    startScreen.style.display = "block"; // Shows Rules 
+    header.style.display = "block"; // Shows Header
+    quizQuestions.style.display = "none"; // Hide Quiz Questions Page
+    finalScorePage.style.display = "none";   // Hide Final Core Page 
 
-// Click event listener to submit button
-//    var initial = initialInput.value.trim()
-//    saveHighscore(initial)
-//    redirect to highscore page
+    var startScore = 0; // Starting time 
+    timer.textContent = "Time: " + startScore; // Holder text in nav bar 
+}
+
+// SHOW QUESTIONS
+function showQuestions() {
+    var quiz = questions[questionIndex];
+
+    quizQuestionTitle.innerHTML = quiz.quizQuestionTitle;
+    choice1.innerHTML = quiz.one;
+    choice1.setAttribute("data-answer", quiz.one);
+    choice2.innerHTML = quiz.two;
+    choice2.setAttribute("data-answer", quiz.two);
+    choice3.innerHTML = quiz.three;
+    choice3.setAttribute("data-answer", quiz.three);
+    choice4.innerHTML = quiz.four;
+    choice4.setAttribute("data-answer", quiz.four);
+}
+
+////////////EVENT LISTENERS////////////////
+
+// START QUIZ - WORKS 
+startButton.addEventListener("click", function () {
+    startQuiz()
+    console.log("start")
+})
---
+++ js/questions.js
@@ -0,0 +1,12 @@
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include",
+        choices: ["strings", "booleans", "alerts"],
+        answer: "alerts"
+    },
+    {
+        title: "Arrays in Javascript can be used to store ___",
+        choices: ["quotes", "curly brackets", "parenthesis", "squaare brackets"],
+        answer: "parenthesis"
+    },
+]
