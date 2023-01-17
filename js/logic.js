ab100c66ff057dbe2327b9dc486d047210ce3e0b
iresuji <iresuji@hotmail.com>
2023-01-17 21:39:07 +0000

    start button clicks

--- js/logic.js
+++ js/logic.js
@@ -1,20 +1,51 @@
 //VARIABLE NEEDED
-var startButton = document.getElementById("start")
+// var body = document.body;
+var header = document.querySelector(".header");
+var score = document.querySelector(".score");
+var submitButton = document.getElementById("start");
 
-var quizQuestions = document.getElementById("questions");
-var startScreen = document.getElementById("start-screen");
-var timer = document.getElementById("timer"); // Timer Variable
-var quizQuestionTitle = document.getElementById("question-title")
+var quizQuestionHeader = document.getElementById("question-title");
+var choice1 = document.getElementById("one");
+var choice2 = document.getElementById("two");
+var choice3 = document.getElementById("three");
+var choice4 = document.getElementById("four");
+var correct = document.getElementById("correct");
+var answerResponse = document.getElementById("answerResponse");
+
+var finalScoreIs = document.getElementById("final-score");
+var quizQuestionsPage = document.getElementById("questions");
+var questionButton = document.querySelector(".choices");
+
+var quizChallengePage = document.getElementById("start-screen");
+var finalScorePage = document.getElementById("end-screen");
+var highScoreButtons = document.getElementById("wrapper");
+
+var initialButton = document.getElementById("start");
+var initials = document.getElementById("initials");
+var initialInput = document.getElementById("initialInput");
+
+var allDone = document.getElementById("allDone");
+var allDoneButtons = document.getElementById("submit");
+
+var timer = document.getElementById("timer"); // Timer Variable 
+
+var startScore = 0;
+var questionIndex = 0;
+
+
+//QUESTIONS
+
+
 
 //FUNCTIONS
 
 
 // Start quiz
 function startQuiz() {
-    startScreen.style.display = "none"; // Hide Rules 
-    quizQuestions.style.display = "block"; // Show Quiz Questions Page
+    quizChallengePage.style.display = "none"; // Hide Rules 
+    quizQuestionsPage.style.display = "block"; // Show Quiz Questions Page
 
-    secondsLeft = 80; // seconds in Timer 
+    secondsLeft = 90; // seconds in Timer 
 
     var timerInterval = setInterval(function () {
         secondsLeft--;
@@ -26,36 +57,10 @@ function startQuiz() {
     }, 1000);
 }
 
-// First page 
-function codeQuizChallenge() {
-    startScreen.style.display = "block"; // Shows Rules 
-    header.style.display = "block"; // Shows Header
-    quizQuestions.style.display = "none"; // Hide Quiz Questions Page
-    finalScorePage.style.display = "none";   // Hide Final Core Page 
-
-    var startScore = 0; // Starting time 
-    timer.textContent = "Time: " + startScore; // Holder text in nav bar 
-}
-
-// SHOW QUESTIONS
-function showQuestions() {
-    var quiz = questions[questionIndex];
-
-    quizQuestionTitle.innerHTML = quiz.quizQuestionTitle;
-    choice1.innerHTML = quiz.one;
-    choice1.setAttribute("data-answer", quiz.one);
-    choice2.innerHTML = quiz.two;
-    choice2.setAttribute("data-answer", quiz.two);
-    choice3.innerHTML = quiz.three;
-    choice3.setAttribute("data-answer", quiz.three);
-    choice4.innerHTML = quiz.four;
-    choice4.setAttribute("data-answer", quiz.four);
-}
-
 ////////////EVENT LISTENERS////////////////
 
 // START QUIZ - WORKS 
-startButton.addEventListener("click", function () {
+submitButton.addEventListener("click", function () {
     startQuiz()
     console.log("start")
 })
